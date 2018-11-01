7996364f1abb0728f46848eee454096df2c5021b
Hoang Duc Quan <rocker.gryphon@gmail.com>
2018-11-01 15:43:27 +0700

    add i18n to login screen. change style in main.css and change register screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,56 +5,82 @@
 @endsection
 
 @section('content')
-{!! Form::open(['method' => 'POST', 'route' => 'login', 'class' => 'login100-form validate-form flex-sb flex-w']) !!}
+{!! Form::open([
+    'method' => 'POST', 
+    'route' => 'login', 
+    'class' => 'login100-form validate-form flex-sb flex-w'
+]) !!}
     <span class="login100-form-title p-b-53">{{ trans('login.login') }}</span>
     
     <a href="#" class="btn-face m-b-20">
         <i class="fa fa-facebook-official"></i>
-        Facebook
+        {{ trans('login.facebook') }}
     </a>
 
 	<a href="#" class="btn-google m-b-20">
 		<img src="{{ asset('images/icon-google.png') }}" alt="GOOGLE">
-	    Google
+	    {{ trans('login.google') }}
 	</a>
     
     <div class="p-t-31 p-b-9">
-        {!! Form::label('email', trans('login.email'), ['class' => 'txt1']) !!}
+        {!! Form::label('email', trans('login.email'), [
+            'class' => 'txt1',
+        ]) !!}
     </div>
 
     <div class="wrap-input100 validate-input">
-        {!! Form::email('email', '', ['class' => 'input100', 'id' => 'email', 'value' => "{{ old('email') }}", 'required' => 'required', 'autofocus' => 'autofocus']) !!}
-
-        @if ($errors->has('email'))
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->first('email') }}</strong>
-            </span>
-        @endif
+        {!! Form::email('email', '', [
+            'class' => 'input100', 
+            'id' => 'email', 
+            'value' => old('email'), 
+            'required' => 'required', 
+            'autofocus' => 'autofocus',
+        ]) !!}
     </div>
+
+    @if ($errors->has('email'))
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $errors->first('email') }}</strong>
+        </span>
+    @endif
     
     <div class="p-t-31 p-b-9">
-        {!! Form::label('password', trans('login.password'), ['class' => 'txt1']) !!}
+        {!! Form::label('password', trans('login.password'), [
+            'class' => 'txt1',
+        ]) !!}
     </div>
 
     <div class="wrap-input100 validate-input">
-        {!! Form::password('password', ['class' => 'input100', 'id' => 'password', 'required' => 'required']) !!}
+        {!! Form::password('password', [
+            'class' => 'input100', 
+            'id' => 'password', 
+            'required' => 'required',
+        ]) !!}        
+    </div>
 
-        @if ($errors->has('password'))
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->first('password') }}</strong>
-            </span>
-        @endif
+    @if ($errors->has('password'))
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $errors->first('password') }}</strong>
+        </span>
+    @endif
+
+    <div class="container-login100-form-btn m-t-17">
+        {!! Form::checkbox('remember', '', [
+            'class' => 'form-check-input', 
+            'id' => 'remember',
+        ]) !!}
+
+        {!! Form::label('remember', trans('login.rememberme'), [
+            'class' => 'fs-17',
+        ]) !!}
+
     </div>
 
     <div class="container-login100-form-btn m-t-17">
-        {!! Form::checkbox('remember', '', ['class' => 'form-check-input', 'id' => 'remember']) !!}
-
-        {!! Form::label('remember', trans('login.rememberme'), ['class' => 'fs-17']) !!}
-
-    </div>
-
-    <div class="container-login100-form-btn m-t-17">
-        {!! Form::button(trans('login.login'), ['class' => 'login100-form-btn', 'type' => 'submit']) !!}
+        {!! Form::button(trans('login.login'), [
+            'class' => 'login100-form-btn', 
+            'type' => 'submit',
+        ]) !!}
 
         <a class="txt2 fs-17" href="{{ route('password.request') }}">
             {{ trans('login.forgot') }}
