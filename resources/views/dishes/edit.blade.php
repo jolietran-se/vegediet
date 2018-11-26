55f4eddc9de96a136e1d1c112778a1015f1cdd1f
phuongyeol <phuongtran99.k60@gmail.com>
2018-11-26 15:22:17 +0700

    add edit

---
+++ resources/views/dishes/edit.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('head')
+@endsection
+
+@section('content')
+    <div class="content page">
+        <div class="container_12">
+            <div class="grid_12">
+                <!-- Content -->
+                <!-- Edit detail dish -->
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('foot')
+@endsection
